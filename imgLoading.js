9cab52b845eb4f7459b9df434a3869a3e2fd991e
tan <782093540@qq.com>
2017-08-29 14:45:29 +0800

    图片加载器

---
+++ imgLoading.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Administrator on 2017/8/29.
+ */
+/**
+ * Created by Administrator on 2017/8/29.
+ * 图片加载器
+ *
+ */
+(function ($) {
+    $.fn.extend({
+        imgLoading:function () {
+            this.each(function (k,v) {
+                var _this = $(this);
+                var img = new Image();
+                img.src = _this.attr('data');
+                img.onload = function (){ //异步加载回调
+                    _this.attr('src',_this.attr('data'));
+                }
+            })
+        }
+    })
+
+
+})(jQuery)
+
+$('img').imgLoading();
